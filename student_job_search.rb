8d1f0912967066848420cd54a094884467dc0eee
michaels107 <michaels.107@osu.edu>
2020-06-12 13:14:25 -0400

    added method to get some more data to refine search further.

--- student_job_search.rb
+++ student_job_search.rb
@@ -1,22 +1,60 @@
 # frozen_string_literal: true
+
 # Created 6/09/2020 by Duytan Tran
+# Edited 6/11/2020 by Sean Michaels
+# Edited 6/12/2020 by Sean Michaels
 # Scrapes the OSU student job board search at https://sfa.osu.edu/jobs/job-board
 # for available listings, outputting them to the console in a quick summarized manner with
 # links to each specific listing for additional details.
 require 'mechanize'
 
+# Created 6/12/2020 by Sean Michaels
+# Method that gets what the user whats to filter their results by
+def filter
+  print 'Enter keywords(if no keyword press enter): '
+  data = []
+  data.push gets.chomp
+  puts 'Would you like to filter by Off Campus[Y/N]:'
+  if gets.chomp.eql? 'Y'
+    data.push true
+  else
+    data.push false
+  end
+  puts 'Would you like to filter by On Campus[Y/N]:'
+  if gets.chomp.eql? 'Y'
+    data.push true
+  else
+    data.push false
+  end
+  puts 'Would you like to filter by Work from Home[Y/N]:'
+  if gets.chomp.eql? 'Y'
+    data.push true
+  else
+    data.push false
+  end
+  data
+end
+
 # Created 6/09/2020 by Duytan Tran
 # Edited 6/11/2020 by Sean Michaels : Created it so that it will search for keywords
+# Edited 6/12/2020 by Sean Michaels : Added method to ask user for data for their search
 # Scrapes Student Job listings and returns them in an array of hashes
 def get_student_job_listings
   url = 'https://sfa.osu.edu/jobs/job-board'
   puts 'Scraping ' + url + '...'
-  print 'Enter keywords(if no keyword press enter): '
-  keyword = gets.chomp
   a = Mechanize.new { |agent| agent.user_agent_alias = 'Windows Firefox' }
   search_page = a.get(url)
   search_form = search_page.forms.last
-  search_form.search = keyword
+  data = filter
+  i = 1
+  search_form.search = data[0]
+  search_form.checkboxes.map do |box|
+    if data[i].eql? true
+      box.click
+    end
+    puts search_form.inspect
+    i += 1
+  end
   unparsed_job_list = a.submit(search_form, search_form.buttons.first)
   parsed_job_list = Nokogiri::HTML(unparsed_job_list.body)
   jobs = []
